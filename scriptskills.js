f8225a8811b23b951641bcfb2fb7b09897db517a
Jake Palmer <jake.palmer@apertureinnovations.co.uk>
2023-01-23 14:26:36 +0000

    Local Copy to push to main

---
+++ scriptskills.js
@@ -0,0 +1,30 @@
+$(".navHamBurger").click(function() {
+  $(".navNavItem").toggleClass("navItemHamDown");
+  $(".navNavGroup").toggleClass("checkDisplay");
+  $(".navHamBurger").toggleClass("navHamBurgerDown");
+});
+
+
+function backToTop() {
+  $('html').animate({ scrollTop: 0 }, 'fast');
+  document.documentElement.scrollTop = 0;
+}
+
+function showSources() {
+  $("#sources").toggleClass("sources");
+  window.scrollTo(0,document.body.scrollHeight);
+}
+
+var currentModalState = false;
+
+function modalOpenClose(number) {
+  var modalgroup = "#modal" + number;
+  if (currentModalState === false) {
+    currentModalState = true;
+    $(modalgroup).toggleClass("display-i-block");
+  } else {
+    currentModalState = false;
+    $(modalgroup).toggleClass("display-i-block");
+  }
+
+}
